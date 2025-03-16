6d611bd2bfb484e6df7489f9086a76f180660e78
Steffen-Svenningsen <stef7625@gmail.com>
2025-03-16 13:44:52 +0100

    Implement try catch

--- server.js
+++ server.js
@@ -16,29 +16,45 @@ const io = require('socket.io')(3001, {
 
 io.on("connection", socket => {
     socket.on('get-document', async documentId => {
-        const document = await findOrCreateDocument(documentId)
-        socket.join(documentId)
-        socket.emit('load-document', document.data)
+        try {
+            const document = await findOrCreateDocument(documentId)
+            socket.join(documentId)
+            socket.emit('load-document', document.data)
+    
+            socket.on('send-changes', delta => {
+                socket.broadcast.to(documentId).emit("receive-changes", delta)
+            })
 
-        socket.on('send-changes', delta => {
-            socket.broadcast.to(documentId).emit("receive-changes", delta)
-        })
-
-        socket.on("save-document", async data => {
-            await Document.findByIdAndUpdate(documentId, { data })
-        })
+            socket.on("save-document", async data => {
+                try {
+                    await Document.findByIdAndUpdate(documentId, { data })
+                } catch (error) {
+                    console.log("Error saving document:", error)
+                    socket.emit('database-error', { message: 'Failed to save document. Please try again later.' })
+                }
+            })
+        } catch (error) {
+            console.error("Error loading document:", error)
+            socket.emit('database-error', { message: 'Failed to load document. Database connection issue.' })
+        }
     })
 })
 
 const defaultValue = ""
 
 async function findOrCreateDocument(id) {
-    if (id == null) return
+    if (id == null) throw new Error("Document ID is required")
 
-    const document = await Document.findById(id)
-    if (document) return document
-    return await Document.create({
-        _id: id,
-        data: defaultValue
-    })
+    try {
+        const document = await Document.findById(id)
+        if (document) return document
+        return await Document.create({
+            _id: id,
+            data: defaultValue
+        })
+    } catch (error) {
+        console.error("Error finding or creating document:", error)
+        throw error
+    }
+
 }
